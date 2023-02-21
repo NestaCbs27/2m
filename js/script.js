8b01f2410dd217d3355f3190b4d0553369947684
NestaCbs27 <97601193+NestaCbs27@users.noreply.github.com>
2023-02-21 13:34:05 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,4 +1,43 @@
 
+let jours = ["Dimanche","Lundi", "Mardi","Mercredi","Jeudi","Vendredi","Samedi"]
+let mois = ["Janvier","Février", "Mars","Avril","Mai","Juin","Juillet","Août","Septembre","Octobre","Novembre","Décembre"]
+
+$( document ).ready(function() {
+    document.getElementById('date').valueAsDate = new Date();
+    setDate(document.getElementById('date'))
+});
+
+function setDate(date){
+    let dayNumber = date.valueAsDate.getDay();
+    let dayLabel = jours[dayNumber];
+    let day =  date.value.split('-')[2];
+    let monthNumber = date.value.split('-')[1];
+    let year =  date.value.split('-')[0];
+
+    if(monthNumber.startsWith('0')){
+        monthNumber = monthNumber.replace('0','')
+    }
+
+    let month = mois[monthNumber];
+
+    let labelDate = dayLabel + ' ' + day + ' ' + month + ' ' + year
+
+    if(day === 0 ){
+        // dimanche
+    }
+
+   $('#labelDate').html(labelDate)
+
+   
+}
+
 function goToReservations(){
     location.href = 'reservations.html';
+}
+
+function chooseReservation(id){
+    if($('#'+id).hasClass('dispo')){
+        $('#'+id).removeClass('dispo');
+        $('#'+id).addClass('nondispo')
+    }
 }
